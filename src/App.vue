44e65e1a64217962340346b6545aa54f3c24464c
maofu <1220485971@qq.com>
2023-10-17 09:56:40 +0800

    test

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import test from './components/test.vue';
+import Test from './components/test.vue';
+
+</script>
+
+<script setup>
+import { ref } from 'vue';
+
+const x = ref(0)
+const awesome = ref(0)
+
+function increment() {
+  x.value ++
+}
+</script>
+
+<template>
+  <header>
+  </header>
+
+  <main>
+    <!-- <TheWelcome /> -->
+    <p>Using text interpolation: {{x ? "xxxx" : "yyyy"}}</p>
+    <p>Using v-html directive: <span v-html="rawHtml"></span></p>
+    <button @click="increment">{{ x }}</button>
+    <button @click.middle="awesome = !awesome">Toggle</button>
+    <h1 v-if="awesome">Vue is awesome!</h1>
+    <h1 v-else>Oh no 😢</h1>
+  </main>
+  <Test></Test>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/test.vue
@@ -0,0 +1,45 @@
+<template>
+    <!-- static -->
+    <!-- <greeting-message name="John" last-name="Doe" /> -->
+  
+    <!-- dynamic with data properties -->
+    <!-- <greeting-message :name="fName" :last-name="lName" /> -->
+    <p v-bind:class="['bold', 'italic', isValid ? 'valid' : 'invalid']">{{ fName }}</p>
+    <a v-bind:href="link">Vue {{ version }} API Reference</a>
+    <p>{{ link }}</p>
+    <p v-pre>{{ link }}</p>
+    <p>name: {{ name }}</p>
+    <input type="text" @input="getInput">
+  </template>
+  
+  <script>
+  //import GreetingMessage from './components/GreetingMessage'
+  
+  export default {
+    //components: { GreetingMessage },
+    data() {
+      return {
+        fName: 'Jane',
+        lName: 'Doe',
+        link: 'https://vuejs.org/api/',
+        dataProperty: 'invalid',
+        isValid: false,
+        version: 3,
+        name:''
+      }
+    },
+
+    methods: {
+      getInput() {
+        this.name = event.target.value
+      }
+    }
+  }
+  </script>
+
+  <style>
+    .bold    { font-weight: bolder }
+    .italic  { font-style:  italic }
+    .valid   { color: forestgreen  }
+    .invalid { color: crimson      }
+</style>
